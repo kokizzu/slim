c3b152cd7a1894ae6970a420f45db38818d3b081
drdr xp <drdr.xp@gmail.com>
2019-07-05 15:16:00 +0800

    refactor: slimtrie: create small old data set for compatibility test

--- trie/testdata/makedata.go
+++ trie/testdata/makedata.go
@@ -2,6 +2,23 @@ package main
 
 import "github.com/openacid/slim/trie"
 
+var (
+	keys11 = []string{
+		"abc",
+		"abcd",
+		"abcdx",
+		"abcdy",
+		"abcdz",
+		"abd",
+		"abde",
+		"bc",
+		"bcd",
+		"bcde",
+		"cde",
+	}
+)
+
 func main() {
+	trie.MakeMarshaledData("slimtrie-data-11-%s", keys11)
 	trie.MakeMarshaledData("slimtrie-data-%s", nil)
 }
